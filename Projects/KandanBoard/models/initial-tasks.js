fab3a305e6d2fe0e2a6d7909c44d97efa927ee99
jhcse5189 <jhcse5189@gmail.com>
2020-01-16 20:29:13 +0000

    MVC without MongoDB ~/Projects/KanbanBoard/ - M: models as ./models/initial-tasks.js - V: views as ./views/index.pug - C: controllers as ./controllers/index.js to ./routes/index.js

---
+++ Projects/KandanBoard/models/initial-tasks.js
@@ -0,0 +1,8 @@
+exports.getTasks = () => {
+    var tasks = {
+        todo: ["design mode", "integration test"],
+        inProgress: ["develop main page"],
+        done: ["design main page"]
+    };
+    return tasks;
+};
